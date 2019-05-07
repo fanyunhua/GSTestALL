e0a87d148b0c09c0b5778ee439b7537e399549e3
user1 <3456>
2019-05-07 10:56:38 +0800

    zmc 侧拉

--- app/src/main/java/cn/pfc/pfc523/MainActivity.java
+++ app/src/main/java/cn/pfc/pfc523/MainActivity.java
@@ -1,13 +1,46 @@
 package cn.pfc.pfc523;
 
+import android.content.Intent;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    SlidingPaneLayout sli;
+    ImageView img;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sli=findViewById(R.id.sli);
+        img=findViewById(R.id.img_zjm);
+        lv=findViewById(R.id.lv_menu);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sli.isOpen()){
+                    sli.closePane();
+                }else {
+                    sli.openPane();
+                }
+            }
+        });
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        startActivity(new Intent(MainActivity.this,MyAccountActivity.class));
+                        break;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/cn/pfc/pfc523/MyAccountActivity.java
@@ -0,0 +1,23 @@
+package cn.pfc.pfc523;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MyAccountActivity extends Activity {
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_myaccount);
+
+        img=findViewById(R.id.img_back_1);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
