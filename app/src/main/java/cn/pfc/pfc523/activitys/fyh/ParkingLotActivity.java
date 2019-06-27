5b69c8b698734767673924c88b29d060d288baa5
student <111@123.co>
2019-06-27 10:09:01 +0800

    FINAL

--- app/src/main/java/cn/pfc/pfc523/activitys/fyh/ParkingLotActivity.java
+++ app/src/main/java/cn/pfc/pfc523/activitys/fyh/ParkingLotActivity.java
@@ -2,17 +2,25 @@ package cn.pfc.pfc523.activitys.fyh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import cn.pfc.pfc523.R;
+import cn.pfc.pfc523.config.AppConfig;
+import cn.pfc.pfc523.http.HttpThread;
 
 public class ParkingLotActivity extends AppCompatActivity {
     private ImageView mParkingBackImage;
     private ImageView mParkingImage;
     private TextView mParkingTVCarCount;
     private Button mParkingBtnQuery;
+    private TextView mParkingMontyTv;
 
 
     @Override
@@ -20,13 +28,61 @@ public class ParkingLotActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lot);
         initView();
+        requestData();
+    }
+
+    private void requestData() {
+        try {
+            new HttpThread(ParkingLotActivity.this)
+            .setURL(AppConfig.BASE_URL+"GetStopCar.do")
+            .setJson(new JSONObject().put("UserName","user1"))
+            .setOnRequest(new HttpThread.OnRequest() {
+                @Override
+                public void onOk(JSONObject jsonObject, boolean isOK) throws JSONException {
+                    if(isOK)
+                    {
+                        JSONArray jsonArray = jsonObject.getJSONArray("Free");
+                        mParkingTVCarCount.setText("剩余车位："+jsonArray.length()+"/10");
+                    }
+                }
+            }).run();
+            new HttpThread(ParkingLotActivity.this)
+                    .setURL(AppConfig.BASE_URL+"GetParkRate.do")
+                    .setJson(new JSONObject().put("UserName","user1"))
+                    .setOnRequest(new HttpThread.OnRequest() {
+                        @Override
+                        public void onOk(JSONObject jsonObject, boolean isOK) throws JSONException {
+                            if(isOK)
+                            {
+                                int money = jsonObject.getInt("Money");
+                                mParkingMontyTv.setText("收费标准："+money+"元/小时");
+                            }
+                        }
+                    }).run();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initView() {
-
         mParkingBackImage = (ImageView) findViewById(R.id.mParkingBackImage);
+        mParkingBackImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         mParkingImage = (ImageView) findViewById(R.id.mParkingImage);
         mParkingTVCarCount = (TextView) findViewById(R.id.mParkingTVCarCount);
         mParkingBtnQuery = (Button) findViewById(R.id.mParkingBtnQuery);
+        mParkingMontyTv = (TextView) findViewById(R.id.mParkingMontyTv);
+        mParkingBtnQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestData();
+            }
+        });
+
     }
 }
--- app/src/main/java/cn/pfc/pfc523/activitys/fyh/PeccancyTypeParserActivity.java
+++ app/src/main/java/cn/pfc/pfc523/activitys/fyh/PeccancyTypeParserActivity.java
@@ -2,14 +2,126 @@ package cn.pfc.pfc523.activitys.fyh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import cn.pfc.pfc523.R;
+import cn.pfc.pfc523.config.AppConfig;
+import cn.pfc.pfc523.http.HttpThread;
 
 public class PeccancyTypeParserActivity extends AppCompatActivity {
+    private RadarChart mPCRadarChart;
+    List<String> pcCodeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peccancy_type_parser);
+        initView();
+        requestData();
+    }
+
+    private void requestData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new HttpThread(PeccancyTypeParserActivity.this).
+                            setURL(AppConfig.BASE_URL+"GetAllCarPeccancy.do")
+                            .setJson(new JSONObject().put("UserName","admin"))
+                            .setOnRequest(new HttpThread.OnRequest() {
+                                @Override
+                                public void onOk(JSONObject jsonObject, boolean isOK) throws JSONException {
+                                    if (isOK)
+                                    {
+                                        JSONArray jsonArray = jsonObject.getJSONArray("ROWS_DETAIL");
+                                        for (int i = 0; i <jsonArray.length() ; i++) {
+                                            JSONObject jj = jsonArray.getJSONObject(i);
+                                            pcCodeList.add(jj.getString("pcode"));
+                                        }
+                                    }
+                                }
+                            }).run();
+                    Thread.sleep(200);
+                    new HttpThread(PeccancyTypeParserActivity.this)
+                            .setURL(AppConfig.BASE_URL+"GetPeccancyType.do")
+                            .setJson(new JSONObject().put("UserName","admin"))
+                            .setOnRequest(new HttpThread.OnRequest() {
+                                @Override
+                                public void onOk(JSONObject jsonObject, boolean isOK) throws JSONException {
+                                    if(isOK)
+                                    {
+                                        final Map<String,String> map = new HashMap<>();
+                                        JSONArray jsonArray = jsonObject.getJSONArray("ROWS_DETAIL");
+                                        for (int i = 0; i <jsonArray.length() ; i++) {
+                                            JSONObject jj = jsonArray.getJSONObject(i);
+                                            map.put(jj.getString("pcode"),jj.getString("premarks"));
+                                        }
+                                        Map<String,Integer> mp = new TreeMap<>(new Comparator<String>() {
+                                            @Override
+                                            public int compare(String o1, String o2) {
+                                                return o2.compareTo(o1);
+                                            }
+                                        });
+                                        Set set = new HashSet();
+                                        final List<String> list = new ArrayList();
+                                        for (String cc:pcCodeList) {
+                                            Integer count =  mp.get(cc);
+                                            mp.put(cc,(count==null)?1:count+1);
+                                            if(set.add(cc)) list.add(cc);
+                                        }
+                                        List<Integer> ll = new ArrayList<>();
+                                        for (String cc:list) {
+                                            ll.add(mp.get(cc));
+                                        }
+                                        List<RadarEntry> radarEntries = new ArrayList<>();
+                                        if(ll.size()>=5)
+                                        {
+                                            for (int i = 0; i <5 ; i++) {
+                                                RadarEntry ff = new RadarEntry(ll.get(i),map.get(list.get(i))+"");
+                                                Log.d("ssssssssssss",map.get(list.get(i))+"");
+                                                radarEntries.add(ff);
+                                            }
+                                        }
+                                        RadarDataSet radarDataSet = new RadarDataSet(radarEntries,"");
+                                        radarDataSet.setFillColor(ColorTemplate.COLORFUL_COLORS[0]);
+                                        radarDataSet.setDrawFilled(true);
+                                        RadarData radarData = new RadarData(radarDataSet);
+                                        mPCRadarChart.setData(radarData);
+                                        mPCRadarChart.invalidate();
+                                    }
+                                }
+                            }).run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private void initView() {
+        mPCRadarChart = (RadarChart) findViewById(R.id.mPCRadarChart);
+        pcCodeList = new ArrayList<>();
+        Legend legend = mPCRadarChart.getLegend();
+        legend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
     }
 }
